3943eca284e1cff2096a22dc98d14cea092b0298
Charles <tietjen.charles@gmail.com>
2023-11-29 17:12:41 -0500

    files stripped and rebuilt

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,26 @@
 import * as React from "react"
-import './App.css'
-import 'react-responsive-carousel/lib/styles/carousel.min.css'
 import {
   ChakraProvider,
-  extendTheme,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
   Grid,
-  GridItem,
+  theme,
+  Heading,
+  HStack,
 } from "@chakra-ui/react"
-import { Routes, Route } from "react-router-dom"
-import { Blog } from './pages/Blog'
-import { About } from './pages/About'
-import { Work } from "./pages/Work"
-import { Contact } from "./pages/Contact"
-import { Nav } from "./components/Nav"
-import { KitchenGoblin } from "./pages/KitchenGoblin"
-import { SpacedOut } from "./pages/SpacedOut"
-import { NotDiscord } from "./pages/NotDiscord"
-import { CMS } from './pages/CMS'
-import { GoblinRunner } from "./pages/GoblinRunner"
+import { ColorModeSwitcher } from "./ColorModeSwitcher"
+import { Logo } from "./Logo"
 
-export const App = () => {
-  const [wHeight, setWHeight] = React.useState(window.innerHeight)
-  const theme = extendTheme({
-    config: {
-      initialColorMode: "light",
-    },
-    styles: {
-      global: {
-        body: {
-          bg: 'linear-gradient(180deg, rgba(55,226,213,1) 23%, rgba(162,87,167,1) 71%)',
-          color: '#303030'
-        }
-      },
-    },
-    fonts: {
-      body: "Noto Sans",
-      heading: "Noto Sans",
-    }
-  })
-  React.useEffect(() => {
-    function handleResize() {
-      setWHeight(window.innerHeight) 
-}
-
-    window.addEventListener('resize', handleResize)
-  })
-  return (
-    <ChakraProvider theme={theme}>
-      <Grid
-        templateAreas={[`"content"
-                        "nav"`,
-                      `nav
-                      content`]}
-        h={wHeight}
-        gridTemplateRows={['88% 12%', null, '10% 90%']}
-      >
-        {/* main */}
-        <GridItem w='100%' justifySelf='center' alignSelf='center' area={['content',null,'nav']}>
-          <Routes>
-            <Route path='/blog' element={<Blog />} />
-            <Route path='/work' element={<Work />} />
-            <Route path='/contact' element={<Contact />} />
-            <Route path='/kitchengoblin' element={<KitchenGoblin />} />
-            <Route path='/spacedout' element={<SpacedOut />} />
-            <Route path='/notdiscord' element={<NotDiscord />} />
-            <Route path='/itsbloggintime' element={<CMS />} />
-            <Route path='/goblinrunner' element={<GoblinRunner />} />
-            <Route path="*" element={<About />} />
-          </Routes>
-        </GridItem>
-        {/* nav */}
-        <GridItem zIndex={5} area={['nav', null, 'content']}>
-          <Nav />
-        </GridItem>
-      </Grid>
-    </ChakraProvider>
-  )
-}
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <Box textAlign="center" fontSize="xl">
+        <HStack justify={"space-between"} m={"2vmax"}>
+        <Heading maxH="20vh">Charles Tietjen</Heading>
+        <ColorModeSwitcher justifySelf="flex-end" />
+        </HStack>
+    </Box>
+  </ChakraProvider>
+)
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import { ColorModeScript } from "@chakra-ui/react"
 import * as React from "react"
 import * as ReactDOM from "react-dom/client"
-import { BrowserRouter } from 'react-router-dom'
 import { App } from "./App"
 import reportWebVitals from "./reportWebVitals"
 import * as serviceWorker from "./serviceWorker"
@@ -12,10 +11,10 @@ if (!container) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(container)
 
 root.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <ColorModeScript />
     <App />
-  </BrowserRouter>,
+  </React.StrictMode>,
 )
 
 // If you want your app to work offline and load faster, you can change
